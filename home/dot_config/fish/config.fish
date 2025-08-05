5aa19ec3ddc98f80506d7e3863db7fa41816d5c0
don't <dont@dont.com>
2025-08-05 12:57:46 +0200

    add condition for paths/brew

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,4 +1,8 @@
-#eval "$(/opt/homebrew/bin/brew shellenv)"
+if test (uname) = 'Darwin'
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
 starship init fish | source
 starship preset tokyo-night -o ~/.config/starship.toml
 
+abbr --add sv 'ssh sv'
