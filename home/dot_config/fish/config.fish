7afae443374eec11a4d5b5edc03671b5d2e4e21d
don't <dont@dont.com>
2025-08-08 23:25:11 +0200

    add starship config

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -2,11 +2,21 @@ if test (uname) = 'Darwin'
     eval "$(/opt/homebrew/bin/brew shellenv)"
 end
 
+set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
+#starship init fish | source
 
-set -x STARSHIP_CONFIG "~/.config/starship/starship.toml"
+function starship_transient_prompt_func
+    # Print the time in HH:MM:SS before the prompt character
+    echo -n (date "+%H:%M:%S ") 
+    starship module character
+end
 
 starship init fish | source
+enable_transience
+
+
 zoxide init --cmd cd fish | source
 fzf --fish | source
 
 abbr --add sv 'ssh sv'
+alias vi nvim
